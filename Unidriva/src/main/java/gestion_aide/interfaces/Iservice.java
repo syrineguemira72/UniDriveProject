3dca29a4f7351f24949e3375a8a1985bf56d017f
walidismail1 <walid.ismail@esprit.tn>
2025-02-25 13:34:24 +0100

    final touch

---
+++ Unidriva/src/main/java/gestion_aide/interfaces/Iservice.java
@@ -0,0 +1,11 @@
+package gestion_aide.interfaces;
+
+import java.util.List;
+
+public interface Iservice<T> {
+    public void addEntity(T t);
+    public void deleteEntity(int id,T t);
+    public void updateEntity(int id,T t);
+    public List<T> getallData();
+
+}
