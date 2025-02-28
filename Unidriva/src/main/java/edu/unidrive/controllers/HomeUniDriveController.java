1692c69146c6d2ace451fb8d9ef145953cc0838b
rihab <Rihab.BenHadjSlimen@esprit.tn>
2025-02-28 14:12:16 +0100

    rihab

--- Unidriva/src/main/java/edu/unidrive/controllers/HomeUniDriveController.java
+++ Unidriva/src/main/java/edu/unidrive/controllers/HomeUniDriveController.java
@@ -61,9 +61,39 @@ public class HomeUniDriveController {
             e.printStackTrace();
         }
     }
+    @FXML
     void forum(MouseEvent event){
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("HomePost.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) logoutbtn.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    void aide(MouseEvent event){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("CRUD.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) logoutbtn.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @FXML
+    void lost(MouseEvent event){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Recompense.fxml"));
             Parent root = loader.load();
             Stage stage = (Stage) logoutbtn.getScene().getWindow();
             Scene scene = new Scene(root);
