1fcceb1f91ab46f51a3879349979168db97d0511
walidismail1 <walid.ismail@esprit.tn>
2025-03-05 01:50:00 +0100

    some modifications in gestion_aide

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.unidrivefinal {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.unidrivefinal to javafx.fxml;
-    exports org.example.unidrivefinal;
-}
